2d811e671d9299daac31f6be357d0174efef6d44
Levike34 <l_meadows34@hotmail.com>
2024-06-13 14:20:34 +0900

    match api_key exists or not

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use k3_wasm_macros::http_handler;
-use k3_wasm_sdk::http::{get_auth, Request, Response, StatusCode};
+use k3_wasm_sdk::http::{get, get_auth, Request, Response, StatusCode};
 use k3_wasm_sdk::{set_env_context, ENV_VAR_LEN, ENV_VAR_PTR};
-
 
 #[http_handler]
 pub fn get(_req: Request<Vec<u8>>) -> Response<Vec<u8>> {
     set_env_context();
 
-    let api_key = std::env::var("API_KEY").expect("API_KEY env var must be present.");
+    let api_key = std::env::var("API_KEY");
     let json_endpoint =
         std::env::var("JSON_ENDPOINT").expect("JSON_ENDPOINT env var must be present.");
 
-    let res = get_auth(&json_endpoint, &api_key);
+    let res = match api_key {
+        Ok(api_key) => get_auth(&json_endpoint, &api_key),
+        Err(_) => get(&json_endpoint),
+    };
 
     Response::builder()
         .status(StatusCode::OK)
